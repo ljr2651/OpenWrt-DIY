1efbf21210882870e408d05f4a0829d3599493a7
dreamskr <43511466+dreamskr@users.noreply.github.com>
2021-08-12 19:21:35 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Add luci-theme-rosy
 git clone --depth=1 -b openwrt-18.06 https://github.com/shiyu1314/luci-theme-rosy
